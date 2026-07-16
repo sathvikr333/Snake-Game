3498ecdae5fe3d68abe61fc5113344578d89b7f3
Sathvik R <pratheekr17@gamil.com>
2026-07-16 17:21:12 +0530

    Add persistent high score feature

--- score.py
+++ score.py
@@ -1,9 +1,15 @@
 from turtle import Turtle
+from pathlib import Path
 class scorecard(Turtle):
     def __init__(self):
         super(). __init__()
         self.penup()
         self.score = 0
+        
+
+        with open("high_score.txt") as data:
+            self.high_score = int(data.read())
+
         self.hideturtle()
         self.goto(0,270)
         self.color("white")
@@ -16,12 +22,16 @@ class scorecard(Turtle):
         self.message.color("white")
     
     def update_score(self):
-        self.write(f"Score : {self.score}", align = "center",font = ("Arial",18,"normal"))
+        self.write(f"Score : {self.score}     High Score : {self.high_score} ", align = "center",font = ("Arial",18,"normal"))
     
     def game_over(self):
         
         self.goto(0,0)
         self.write("GAME OVER",align ="center",font = ("Arial",24,"bold"))
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("high_score.txt", mode = "w") as data:
+                data.write(f"{self.high_score}")
         self.show_restart_message()
     def show_restart_message(self):
         self.message.goto(0, -80)
@@ -37,6 +47,11 @@ class scorecard(Turtle):
         
         self.clear()
         self.message.clear()
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("high_score.txt", mode = "w") as data:
+                data.write(f"{self.high_score}")
+
         self.score = 0
         self.goto(0, 270)
         self.update_score()
